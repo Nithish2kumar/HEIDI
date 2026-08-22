bfdaf0bdbb1a74f73208198e3445726e89b55f90
Nithish2Kumar <jnithishkumar2@gmail.com>
2026-08-22 23:45:18 +0530

    Implemented Encryption

---
+++ source/enc.py
@@ -0,0 +1,34 @@
+from PIL import Image
+
+def encode(inI,outI,msg):
+
+    #----Processing the input file-----
+
+    image= Image.open(inI).convert("RGB")#convert each pixel into RGB value
+    message+="<<<END>>>"#to stop the loop while decoding
+    binary=''.join(format(ord(char),'08b') for char in message)#msg to 8bit binary
+    pix=List(image.getdata())#Full RGB value of picture
+
+    if len(binary)>len(pix)*3:#checking whether the image is small or large
+        raise ValueError("Message is too large")
+
+    #----Processing the output file-----
+
+    newPix=[]
+    bitIndex=0#Tells the index msg bit
+
+    for pi in pix:#iterate RGB
+        newPi=[]
+        for value in pi:#iterate the value in RGB
+            if bitIndex<len(binary):
+                value=(value&"0xFE")|int(binary[bitIndex])#Steganography
+                bitIndex+=1
+                newPi.append(value)
+        newPix.append(tuple(newPi))
+
+    image.putdata(newPix)#Modified image
+    image.save(outI)
+    print("Successfully hidden")
+
+
+
